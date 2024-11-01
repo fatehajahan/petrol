b4f146c461a2981b387ff399e7f3eac97e7a98c8
Fatehajahan2002 <fatehajahan2002@gmail.com>
2024-11-01 22:38:01 +0600

    done

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -3,7 +3,7 @@ import logo1 from "../../assets/logo1.png"
 import logo2 from "../../assets/logo2.png"
 import logo3 from "../../assets/logo3.png"
 import logo4 from "../../assets/logo4.png"
-import Container from '../Container/Container'
+import Container from "../Container/Container"
 
 const Logos = () => {
     return (
